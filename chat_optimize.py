b36bbfd70c77f97a1d19e97e478c9ed5c6f381ff
Zach Jaggi <feilen1000@gmail.com>
2016-05-03 03:15:57 -0500

    Vastly overhaul model

--- chat_optimize.py
+++ chat_optimize.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python2
 import re
-import csv
+import unicodecsv as csv
 import numpy as np
 import random
 import json
 import theano
+import theano.tensor as T
 
 from glove import Corpus, Glove
+
+from scipy.stats import norm
 
 from sklearn.preprocessing import Imputer
 from lasagne import layers
@@ -16,8 +19,14 @@ from nolearn.lasagne import NeuralNet
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
 
-max_sentence_length = 23
-number_components = 4
+# 5 seems to do best?
+number_components = 5
+# Include the last few messages from the same conversation, same user in train data
+contextual = True
+# How many max posts previous to check for the same username
+max_context_backstep = 5
+# Drop probablity, set until train/val is stable
+drop_probability = 0.5
 
 logfile = open('After_Dark_Furry_Femboys.jsonl', 'r')
 
@@ -35,16 +44,19 @@ id_lookup = dict()
 for index, message in enumerate(jsons):
     id_lookup[message['id']] = index
 
-def vectify(sentence_array):
-    vectorized = np.empty((len(sentence_array), max_sentence_length, number_components), dtype=np.float32)
-    for index, sentence in enumerate(sentence_array):
-        for w_index, word in reversed(list(enumerate(sentence))):
-            if max_sentence_length - len(sentence) + w_index > 0:
-                #try:
-                vectorized[index][max_sentence_length - len(sentence) + w_index ] = glove.word_vectors[glove.dictionary[word.lower()]]
-                #except:
-                #    vect_sentence.extend([np.nan] * number_components)
-    return vectorized
+# Create a reply chain map
+previous_message = dict()
+for index, message in enumerate(jsons):
+    for jindex, jmessage in enumerate(jsons[index:index+max_context_backstep]):
+        if 'username' in message['from'] and 'username' in jmessage['from'] and (message['from']['username'] == jmessage['from']['username']):
+            previous_message[index] = jindex
+            break
+        else:
+            previous_message[index] = -1
+    #try:
+    #    previous_message[index] = id_lookup[jsons[index]['reply_id']]
+    #except KeyError:
+    #    previous_message[index] = index + 1
 
 # Bundle groups of symbols and make sure words are otherwise alone
 print("Parsing CSV log")
@@ -63,6 +75,38 @@ for row in mycsv:
     texts.append(clean(row[3]).split())
     classes.append(row[0])
 
+# Calculate distribution, to account for 95th percentile of messages.
+max_sentence_length = int(np.mean([len(x) for x in texts]) + (norm.ppf(0.95) * np.std([len(x) for x in texts])))
+
+def vectify(sentence_array):
+    vectorized = np.zeros((len(sentence_array), max_sentence_length), dtype=np.int32)
+    for index, sentence in enumerate(sentence_array):
+        # Construct a list of word indexes, such the current classified message is at the end, and previous messages are in every spot leading up to it.
+        # Ex: "hi how are you ." "hi there" -> ". are you hi there"
+        sentence_index = max_sentence_length
+        cur_chain_message_index = index
+        if contextual:
+            while sentence_index > 0:
+                try:
+                    working_sentence = texts[cur_chain_message_index]
+                except IndexError:
+                    pass # Just feed in the same sentence I guess
+                # Overwrite portion of sentence
+                np.put(vectorized[index], np.arange(len(working_sentence)) + (sentence_index - len(working_sentence)), [glove.dictionary[word.lower()] for word in working_sentence], mode='clip')
+                sentence_index -= len(working_sentence)
+                # Move back one message until we've filled it
+                try:
+                    cur_chain_message_index = previous_message[cur_chain_message_index]
+                except KeyError:
+                    pass
+        else:
+            working_sentence = texts[cur_chain_message_index]
+            np.put(vectorized[index], np.arange(len(working_sentence)) + (sentence_index - len(working_sentence)), [glove.dictionary[word.lower()] for word in working_sentence], mode='clip')
+
+
+    return vectorized
+
+
 corpus = Corpus()
 try:
     print("Loading pretrained corpus...")
@@ -79,116 +123,85 @@ try:
 except:
     print("Training GloVe vectors...")
     # More epochs seems to make it worse
-    glove.fit(corpus.matrix, epochs=200, no_threads=4, verbose=True)
+    glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
     glove.add_dictionary(corpus.dictionary)
     glove.save("glove.p")
 
 # Convert input text
 X = vectify(texts)
-X_context = np.empty((len(X), max_sentence_length * 2, number_components), dtype=np.float32)
-# Feed context. Context being 'replied to', 'prior message' or 'NaN'
-for index, vector in enumerate(X):
-    try:
-        X_context[index] = np.concatenate((X[index], (X[id_lookup[jsons[index]['reply_id']]])))
-    except (KeyError, IndexError):
-        try:
-            X_context[index] = np.concatenate((X[index], (X[index + 1])))
-        except IndexError:
-            pass
+y = np.array([x == u'1' for x in classes]).astype(np.int32)
 
-X = X_context
+X, y, jsons, texts = X[:207458], y[:207458], jsons[:207458], texts[:207458]
 
-#impute input text
-for sentence in X:
-    imp = Imputer(missing_values='NaN', strategy='mean')
-    imp.fit(sentence)
-    sentence = imp.transform(sentence)
+#X, y, jsons, texts, indexes = shuffle(X, y, jsons, texts, np.arange(len(X)))
 
-y = np.array([x is '1' for x in classes]).astype(np.int32)
+def print_accurate_forwards(net, history):
+    X_train, X_valid, y_train, y_valid = net.train_split(X, y, net)
+    y_classified = net.predict(X_valid)
+    print('Accurately forwarded: {}'.format(np.mean([x == y_ and y_ == 1 for x, y_ in zip(y_valid, y_classified)])/np.mean(y_valid)) + ', False Positives: {}'.format(np.mean([x != y_ and y_ == 0 for x, y_ in zip(y_classified, y_valid)])/(np.mean(y_valid))))
 
-#X = X[:207458]
-#y = y[:207458]
-
-# Split the data into train and test instances
-#paired = zip(X, y, texts)
-#shuffle(X, y, texts)
-
-test_X = X[len(X) * 7 / 10:]
-X = X[:len(X) * 7 / 10]
-
-test_y = y[len(y) * 7 / 10:]
-y = y[:len(y) * 7 / 10]
-
-test_texts = texts[len(texts) * 7 / 10:]
-#train_unzip = [np.array(t) for t in zip(*train_instances)]
-#test_unzip = [np.array(t) for t in zip(*test_instances)]
-
-#X = train_unzip[0]
-#y = train_unzip[1]
-
-#test_X = test_unzip[0]
-#test_y = test_unzip[1]
-
-#scaler = StandardScaler()
-#scaler.fit(X)
-#X = scaler.transform(X)
-#test_X = scaler.transform(test_X)
-
-#X = X.reshape(
-#        -1,  # number of samples, -1 makes it so that this number is determined automatically
-#        max_sentence_length * 2,   # 1 color channel, since images are only black and white
-#        number_components # second image dimension (horizontal)
-#    )
-
-#test_X = test_X.reshape(
-#        -1,  # number of samples, -1 makes it so that this number is determined automatically
-#        max_sentence_length * 2,   # 1 color channel, since images are only black and white
-#        number_components # second image dimension (horizontal)
-#    )
-
-# Train over data, produce regression NN, spit out the middle 10% and run statistics
 net = NeuralNet(
-        layers=[  # three layers: one hidden layer
+        layers=[
             ('input', layers.InputLayer),
-            #('drop', layers.DropoutLayer),
+            ('embed', layers.EmbeddingLayer),
+            ('drop', layers.DropoutLayer),
             ('lstm1', layers.LSTMLayer),
             ('lstm2', layers.LSTMLayer),
-#            ('lstm3', layers.LSTMLayer),
+            ('lstm3', layers.LSTMLayer),
             ('output', layers.DenseLayer),
             ],
-        # layer parameters:
-        input_shape = (None, max_sentence_length * 2, number_components),  # this code won't compile without SIZE being set
+        # layers
+        input_shape = (None, max_sentence_length),
+        input_input_var=T.imatrix(),
+
+        embed_input_size = glove.word_vectors.shape[0],
+        embed_output_size = glove.word_vectors.shape[1],
+        embed_W = np.array(glove.word_vectors, dtype=np.float32),
+
+        drop_p = drop_probability,
 
         lstm1_num_units = number_components,
         #lstm1_nonlinearity = tanh,
         lstm2_num_units = number_components,
         #lstm2_nonlinearity = tanh,
-        #lstm3_num_units = number_components,
-        #lstm3_nonlinearity = tanh,
+        lstm3_num_units = number_components,
+        #lstm2_nonlinearity = tanh,
 
-        output_nonlinearity = softmax,  # output layer uses identity function
-        output_num_units = 2,  # this code won't compile without OUTPUTS being set
+        output_nonlinearity = softmax,
+        output_num_units = 2,
 
-        # optimization method:
+        # optimization
         update=adam,
-        update_learning_rate=0.01,
+        update_learning_rate=0.005,
 
-        regression=False,  # If you're doing classification you want this off
-        max_epochs=125,  # more epochs can be good,
-        verbose=1, # enabled so that you see meaningful output when the program runs
+        regression=False,
+        max_epochs=45,
+
+        verbose=1,
+        on_epoch_finished = [print_accurate_forwards],
         )
 
-## Train the network
 net.fit(X, y)
 
-# Test classifier
+## Train and run the network
 classified = net.predict(X)
-error = np.mean( classified != y )
-print('Accuracy on train data: {}'.format(1-error))
-test_classified = net.predict(test_X)
-test_error = np.mean( test_classified != test_y )
-print('Accuracy on test data: {}'.format(1-test_error))
-print('Accurately forwarded: {}'.format(np.mean([x == y and y == 1 for x, y in zip(test_y, test_classified)])/np.mean(test_y)))
-for index, item in enumerate(test_classified):
-    if item == 1:
-        print(test_texts[index])
+with open('chat-optimized.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile)
+    index = 0
+    for index, (json, predicted_class, actual_class) in enumerate(zip(jsons, classified, y)):
+        name = (json['from']['first_name'] if 'first_name' in json['from'] else "") + " " + (json['from']['last_name'] if 'last_name' in json['from'] else "")
+        class_written = ''
+        if predicted_class == 1 and actual_class == 1:
+            class_written = 1
+        if predicted_class == 0 and actual_class == 1:
+            class_written = 'false_negative'
+        if predicted_class == 1 and actual_class == 0:
+            class_written = 'false_positive'
+        spamwriter.writerow(
+            [class_written] +
+            [name] +
+            ([json['from']['username']] if 'username' in json['from'] else ['']) +
+            ([json['text']] if 'text' in json else ['']) +
+            [index]
+        )
+        index += 1
